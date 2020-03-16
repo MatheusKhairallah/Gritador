334e2d2a126fcd046c89d0e061a72ce094b04442
matheus <playermaster10like@gmail.com>
2020-03-16 18:26:55 -0300

    programa c#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite algo: ");
+            string texto=Console.ReadLine();
+            string texto2=texto.ToUpper();
+
+            Console.WriteLine(texto2);
+        }
+    }
+}
